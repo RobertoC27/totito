81c6b0f366954d9e1510c1fd087cb790d87808a4
RobertoC27 <chi13027@uvg.edu.gt>
2016-08-02 13:52:59 -0600

    v3 entregable
    juego completo

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,13 +4,17 @@ var turno=1;
 
 var canvas=document.getElementById("viewport");
 var bt=document.getElementById("btn");
+var win=document.getElementById("ganador");
 var squares= document.getElementsByClassName("square");
 var board=["","","","","","","","",""];
-
-
+var mark="";
+var cl;
 function rev(element){
-	var mark="";
+	
 	if (squares[element].innerHTML=== "") {
+	    if (turno===3) {
+				return 1;
+			}
 	    if (turno===1) {
 	    	mark="X";
 	    	turno=2;
@@ -18,14 +22,13 @@ function rev(element){
 	    	mark="O";
 	    	turno=1;
 	    }
+	    
     squares[element].innerHTML=mark;
     board[element]=mark;
-    render(element,mark);
+    render();
 	}
 	
 }
-
-
 
 squares[0].addEventListener("click",function(){ rev(0);} );
 squares[1].addEventListener("click",function(){ rev(1);} );
@@ -37,16 +40,12 @@ squares[6].addEventListener("click",function(){ rev(6);} );
 squares[7].addEventListener("click",function(){ rev(7);} );
 squares[8].addEventListener("click",function(){ rev(8);} );
 
+bt.addEventListener("click",init);
 
-function paint(e1,e2,e3){
-	squares[e1].style.color="green";
-	squares[e2].style.color="green";
-	squares[e3].style.color="green";
-	turno=3;
-	bt.style.visibility="visible";
-}
 
 function render(){
+
+
 	if (board[0] !== "") {
 		//revisar 1ra fila, diagonal, 1ra columna
 		if (board[1]===board[0]) {
@@ -114,7 +113,42 @@ function render(){
 		}
 	}
 
-	console.log(board);
+	for (var i = squares.length - 1; i >= 0; i--) {
+		if (board[i]==="") {
+			break;
+		}
+	}
+	
+	//cuando esto pasa es que hay un empate
+	if (i === -1) {
+		win.innerHTML=("¡Empate!");
+		init();
+	}
 }
 
 
+function paint(e1,e2,e3){
+	squares[e1].style.color="green";
+	squares[e2].style.color="green";
+	squares[e3].style.color="green";
+	
+	win.innerHTML=("¡Gana el jugador de las "+mark+"'s !");
+	turno=3;
+	bt.style.visibility="visible";
+	win.style.visibility="visible";
+
+
+}
+
+function init(){
+	turno=1;
+	
+	for (var i = squares.length - 1; i >= 0; i--) {
+		board[i]="";
+		squares[i].innerHTML="";
+		squares[i].removeAttribute("style");
+	}
+	
+	bt.style.visibility="hidden";
+	win.style.visibility="hidden";
+}
